6eb9ac1164bd79e72d21ef804c0066b533011968
Tanmay704 <tanmaygunjal2@gmail.com>
2022-02-08 19:26:44 +0530

    total count update

--- ObjectDetection/app/src/main/java/org/pytorch/demo/objectdetection/NewAnalysisActivity.java
+++ ObjectDetection/app/src/main/java/org/pytorch/demo/objectdetection/NewAnalysisActivity.java
@@ -11,10 +11,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.Objects;
 
 public class NewAnalysisActivity extends AppCompatActivity {
-    public long total = 0;
+    public long plastic_total = 0;
+    public long paper_total = 0;
+    public long cardboard_total = 0;
+    public long metal_total = 0;
+    public long thermocol_total = 0;
+    public long glass_total = 0;
     public NewAnalysisActivity(){
         getAllCount();
     }
@@ -34,18 +38,38 @@ public class NewAnalysisActivity extends AppCompatActivity {
 
 
 
-                total = 0;
-
+                plastic_total = 0;
+                paper_total = 0;
+                cardboard_total = 0;
+                metal_total = 0;
+                thermocol_total = 0;
+                glass_total = 0;
                 for(DataSnapshot ds : dataSnapshot1.getChildren()) {
                     for(DataSnapshot ds2 : ds.getChildren()) {
 
                         Long amount = ds2.child("Plastic").child("Count").getValue(Long.class);
-                            total += amount;
+                        plastic_total += amount;
+                        amount = ds2.child("Paper").child("Count").getValue(Long.class);
+                        paper_total += amount;
+                        amount = ds2.child("Cardboard").child("Count").getValue(Long.class);
+                        cardboard_total += amount;
+                        amount = ds2.child("Metal").child("Count").getValue(Long.class);
+                        metal_total += amount;
+                        amount = ds2.child("Thermocol").child("Count").getValue(Long.class);
+                        thermocol_total += amount;
+                        amount = ds2.child("Glass").child("Count").getValue(Long.class);
+                        glass_total += amount;
 
                    }
                 }
-                System.out.println("())))))))))))))))))))))))))))))))))))))))))))))))))))))))))"+total);
-                System.out.println("ABHI_GUNJAL");
+                System.out.println("())))))))))))))))))))))))))))))))))))))))))))))))))))))))))"+cardboard_total);
+                System.out.println("())))))))))))))))))))))))))))))))))))))))))))))))))))))))))"+glass_total);
+                System.out.println("())))))))))))))))))))))))))))))))))))))))))))))))))))))))))"+metal_total);
+                System.out.println("())))))))))))))))))))))))))))))))))))))))))))))))))))))))))"+paper_total);
+                System.out.println("())))))))))))))))))))))))))))))))))))))))))))))))))))))))))"+plastic_total);
+                System.out.println("())))))))))))))))))))))))))))))))))))))))))))))))))))))))))"+thermocol_total);
+
+
             }
 
             @Override
